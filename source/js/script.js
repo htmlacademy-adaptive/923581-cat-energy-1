e180cbcfd89907192a973e67f249acac2f5ed14f
AtomWave <AtomWave@yandex.ru>
2024-03-28 19:46:12 +0300

    активирует меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,16 @@
 //==== Модуь работы с меню (бургер)
 
+let iconMenu = document.querySelector('.icon-menu');
+let navMain = document.querySelector('.menu');
 
+navMain.classList.remove('menu--nojs');
 
-
+iconMenu.addEventListener('click', function () {
+  if (navMain.classList.contains('menu--closed')) {
+    navMain.classList.remove('menu--closed');
+    navMain.classList.add('menu--opened');
+  } else {
+    navMain.classList.remove('menu--opened');
+    navMain.classList.add('menu--closed');
+  }
+});
