38ab4262643fb6140105a2e2cb611601578c7513
Keks <keks@htmlacademy.ru>
2024-04-19 21:58:16 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/script.js
+++ 7/js/script.js
@@ -14,3 +14,9 @@ iconMenu.addEventListener('click', function () {
     navMain.classList.add('menu--opened');
   }
 });
+
+function slide(){
+    let slideValue = document.getElementById("slider").value;
+    document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+    console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+}
